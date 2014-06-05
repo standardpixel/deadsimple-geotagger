70e06586fde6fae29a278bb87f90c244942dbf4b
Eric Gelinas <eric@standardpixel.com>
2014-06-04 21:50:51 -0700

    add dummy flickr data

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+//
+// Dead Fucking Simple Geotagger
+//
+// Arguments:
+//   -f (required) ~ A GPX File (~/Documents/file.gpx)
+//   -t            ~ Time zone offset (-0700 would make 000 which is GMT, pacific time)
+//
+
+'use strict';
+
 var cli     = require('cli'),
     fs      = require('fs'),
     gpsUtil = require('gps-util');
 
 var keyedArgs   = {},
     usageString = 'Usage: > node deadsimple-geotagger -g [path to gpx file]',
+    content     = {},
     rawGeoData;
 
 cli.main(function(args, options) {
@@ -47,7 +58,14 @@ cli.main(function(args, options) {
 
 	gpsUtil.gpxParse(rawGeoData, function(err, gpsData) {
 
+		//
+		// Get Flickr data
+		//
+
+		content['flickr'] = require('./test-data/flickr-people-photos.json');
+
 		console.log('gpsData', gpsData);
+		console.log('flickrData', content['flickr']);
 
 	});
 
